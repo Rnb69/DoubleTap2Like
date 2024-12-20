5412f6c857b6265b89c1cd54dbc550f8fdabeebe
Yash Thakur <59911586+Rnb69@users.noreply.github.com>
2024-12-20 11:59:01 +0530

    Sync b/w bigheart and smallheart
    Added line for updating smallHeart color to red after using double click

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var con =  document.querySelector("#container");
 con.addEventListener("dblclick", function(){
     bigheart.style.transform = 'translate(-50%,-50%) scale(1)'
     setTimeout(heartTimeout, 1000);
+    smallheart.style.color = 'red';
 })
 
 function heartTimeout(){
